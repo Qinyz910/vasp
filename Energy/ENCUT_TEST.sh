feb0569b56dc92786e9c400555bccc82966ff5d5
Qinyz910 <50287772+Qinyz910@users.noreply.github.com>
2021-01-21 16:31:31 +0800

    Update ENCUT_TEST.sh

--- Energy/ENCUT_TEST.sh
+++ Energy/ENCUT_TEST.sh
@@ -1,1 +1,19 @@
-
+#!/bin/bash
+for i in 300 350 400 450 500 550 600 650 700
+do  cat > INCAR << !
+test encut
+ISTART = 1
+ICHARG = 1
+ENCUT = $i eV
+ISMEAR = 0 
+SIGMA = 0.05
+LWAVE = .FALSE
+LCHARGE = .FALSE
+!
+mpirun -np 8 vasp
+rm -f WAVECAR CHGCAR
+echo "ENCUT = $i eV was performed above"
+E= `grep "TOTEN" OUTCAR |awk '{print $5}'`
+cputime= `grep "CPU" OUTCAR |awk '{print $6}'`
+echo $i $E $cputime >> compare.dat
+done
